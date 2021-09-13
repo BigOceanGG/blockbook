1c833a0f0bd1c592b3283e0369c2fd19bb1d967e
bigocean <3408598@qq.com>
2021-09-13 15:21:57 +0800

    modify trx

--- db/txcache.go
+++ db/txcache.go
@@ -116,13 +116,17 @@ func (c *TxCache) GetTransaction(txid string) (*bchain.Tx, int, error) {
 
 func (c *TxCache) GetTransactionSpecific(txid string) (json.RawMessage, error) {
 	var tx *bchain.Tx
+	var h uint32
 	var err error
 	if c.enabled {
-		tx, _, err = c.db.GetTx(txid)
+		tx, h, err = c.db.GetTx(txid)
 		if err != nil {
 			return nil, err
 		}
 		if tx != nil {
+			_, bestheight, _ := c.is.GetSyncState()
+			tx.Confirmations = bestheight - h + 1
+			c.metrics.TxCacheEfficiency.With(common.Labels{"status": "hit"}).Inc()
 			glog.Info("GetTransactionSpecific by cache : ", txid)
 			return c.chain.GetTransactionSpecific(tx)
 		}
@@ -131,6 +135,55 @@ func (c *TxCache) GetTransactionSpecific(txid string) (json.RawMessage, error) {
 	tx, err = c.chain.GetTransaction(txid)
 	if err != nil {
 		return nil, err
+	}
+	if c.chainType == bchain.ChainTronType {
+		tx.Confirmations = c.is.BestHeight - h + 1
+	}
+	c.metrics.TxCacheEfficiency.With(common.Labels{"status": "miss"}).Inc()
+	// cache only confirmed transactions
+	if tx.Confirmations > 0 {
+		if c.chainType == bchain.ChainBitcoinType {
+			ta, err := c.db.GetTxAddresses(txid)
+			if err != nil {
+				return nil, err
+			}
+			switch {
+			case ta == nil:
+				// the transaction may not yet be indexed, in that case:
+				if tx.BlockHeight > 0 {
+					// Check if the tx height value is set.
+					h = tx.BlockHeight
+				} else {
+					// Get the height from the backend's bestblock.
+					h, err = c.chain.GetBestBlockHeight()
+					if err != nil {
+						return nil, err
+					}
+				}
+			default:
+				h = ta.Height
+			}
+		} else if c.chainType == bchain.ChainEthereumType {
+			h, err = eth.GetHeightFromTx(tx)
+			if err != nil {
+				return nil, err
+			}
+		} else if c.chainType == bchain.ChainTronType {
+			h, err = trx.GetHeightFromTx(tx)
+			if err != nil {
+				return nil, err
+			}
+			tx.BlockHeight = h
+		} else {
+			return nil, errors.New("Unknown chain type")
+		}
+		if c.enabled {
+			err = c.db.PutTx(tx, h, tx.Blocktime)
+			// do not return caching error, only log it
+			if err != nil {
+				glog.Warning("PutTx ", tx.Txid, ",error ", err)
+			}
+		}
 	}
 	return c.chain.GetTransactionSpecific(tx)
 }
