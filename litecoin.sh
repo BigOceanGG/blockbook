652847b9fb6fb740c64c67b9cebf38238bade9c6
bigocean <3408598@qq.com>
2021-09-18 09:06:39 +0800

    modify trx

---
+++ litecoin.sh
@@ -0,0 +1,1 @@
+./blockbook -workers=8 -sync -blockchaincfg=build/litecoin.json -datadir=/home/admin/coins/litecoin/dbs -internal=:19004 -public=:19104 -logtostderr > ~/coins/litecoin/litecoin.log 2>&1 &
