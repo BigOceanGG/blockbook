6471920f5500e471b372cb93d21ab2370ebdd7bd
bigocean <3408598@qq.com>
2021-09-18 14:04:07 +0800

    modify trx

--- trx.sh
+++ trx.sh
@@ -1,1 +1,1 @@
-./blockbook -workers=8 -sync -blockchaincfg=build/trx.json -datadir=/home/admin/coins/trx/dbs -internal=:19001 -public=:19101 -logtostderr > ~/coins/trx/trx.log 2>&1 &
+./blockbook -workers=64 -sync -blockchaincfg=build/trx.json -datadir=/home/admin/coins/trx/dbs -internal=:19001 -public=:19101 -logtostderr > ~/coins/trx/trx.log 2>&1 &
