95d9736d5ae146e45d0ba2c25328ae567846ea37
bigocean <3408598@qq.com>
2021-09-22 14:23:29 +0800

    add sh

--- trx.sh
+++ trx.sh
@@ -1,1 +1,1 @@
-./blockbook -workers=128 -sync -blockchaincfg=build/trx.json -datadir=/home/admin/coins/trx/dbs -internal=:19001 -public=:19101 -logtostderr > ~/coins/trx/trx.log 2>&1 &
+./blockbook -workers=512 -sync -blockchaincfg=build/trx.json -datadir=/home/admin/coins/trx/dbs -internal=:19001 -public=:19101 -logtostderr > ~/coins/trx/trx.log 2>&1 &
