79fb16dbc16684e45f484ccbf08cec67be31fcbd
bigocean <3408598@qq.com>
2021-09-22 15:53:49 +0800

    add sh

--- watch.sh
+++ watch.sh
@@ -4,14 +4,15 @@ PRO_NAME = bitcoin.json
 export LD_LIBRARY_PATH = ./
 
 while true; do
+
         NUM = `ps aux | grep ${PRO_NAME} | grep -v grep |wc -l`
         if [ "${NUM}" -lt "1" ]; then
             echo "${PRO_NAME} was killed"
             ./blockbook -workers=1 -sync -blockchaincfg=build/bitcoin.json -datadir=/home/admin/coins/bitcoin/dbs -internal=:19000 -public=:19100 -logtostderr > ~/coins/bitcoin/bitcoin.log 2>&1 &
             sleep 10
+        else
+            echo "watch ${PRO_NAME}"
+            sleep 60
         fi
-
-        echo "watch ${PRO_NAME}"
-        sleep 60
 
 done
