b793fbfc123e7938c6cec6356206029ddc8db793
bigocean <3408598@qq.com>
2021-09-29 09:08:19 +0800

    add sh

--- trx.sh
+++ trx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FILE=~/coins/trx/trx_$(date "+%Y%m%d%H%M%S").log
-./blockbook -workers=128 -sync -blockchaincfg=build/trx.json -datadir=/home/admin/coins/trx/dbs -internal=:19001 -public=:19101 -logtostderr > ${FILE} 2>&1 &
+./blockbook -workers=512 -sync -blockchaincfg=build/trx.json -datadir=/home/admin/coins/trx/dbs -internal=:19001 -public=:19101 -logtostderr > ${FILE} 2>&1 &
 rm ~/coins/trx/trx.log
 ln -s ${FILE} ~/coins/trx/trx.log
