d8ae8a9e618a9a7343a5bc7b5a203cd30bc24c19
bigocean <3408598@qq.com>
2021-10-11 09:47:15 +0800

    add sh

--- watch.sh
+++ watch.sh
@@ -6,18 +6,14 @@ while true; do
 
         NUM=`free | awk '/Mem/ {print $7}'`
         if [ ${NUM} -lt 1000000 ]; then
-            PID=`ps aux | grep ethereum.json| grep -v grep|awk '{print $2}'`
-            if  [ $PID ]; then
-              echo "$(date "+%Y-%m-%d %H:%M:%S") ethereum was killed"
-              kill -2 ${PID}
-              echo "$(date "+%Y-%m-%d %H:%M:%S") kill -2 ... ${NUM}"
-              sleep 120
-              kill -9 ${PID}
-              echo "$(date "+%Y-%m-%d %H:%M:%S") kill -9 ... ${NUM}"
-              sleep 10
-              bash ethereum.sh
-              echo "$(date "+%Y-%m-%d %H:%M:%S") start ..."
-            fi
+            pkill -2 blockbook
+            echo "$(date "+%Y-%m-%d %H:%M:%S") pkill -2 ... ${NUM}"
+            sleep 120
+            pkill -9 blockbook
+            echo "$(date "+%Y-%m-%d %H:%M:%S") pkill -9 ... ${NUM}"
+            sleep 10
+            bash ethereum.sh
+            echo "$(date "+%Y-%m-%d %H:%M:%S") start ..."
        else
             echo "$(date "+%Y-%m-%d %H:%M:%S") watch ... ${NUM}"
         fi
