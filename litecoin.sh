a2b9ea3992956a88c0f11d71fc327931cbd46d7c
ocean <3408598@qq.com>
2021-11-23 15:23:17 +0800

    modify sh

--- litecoin.sh
+++ litecoin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FILE=~/coins/litecoin/litecoin_$(date "+%Y%m%d%H%M%S").log
-./blockbook -workers=8 -sync -blockchaincfg=build/litecoin.json -datadir=/home/admin/coins/litecoin/dbs -internal=:19004 -public=:19104 -logtostderr > ${FILE} 2>&1 &
+./blockbook -workers=1 -sync -blockchaincfg=build/litecoin.json -datadir=/home/admin/coins/litecoin/dbs -internal=:19004 -public=:19104 -logtostderr > ${FILE} 2>&1 &
 rm ~/coins/litecoin/litecoin.log
 ln -s ${FILE} ~/coins/litecoin/litecoin.log
