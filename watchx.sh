fd1ca86c65fba6e27b71e5f33db97237b64ab4df
ocean <3408598@qq.com>
2021-12-02 16:59:34 +0800

    add sh

--- watchx.sh
+++ watchx.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-PRO_NAME=bcash.json
+PRO_NAME=dogecoin.json
 export LD_LIBRARY_PATH=./
 
 while true; do
         NUM=`ps aux | grep ${PRO_NAME} | grep -v grep |wc -l`
         if [ "${NUM}" -lt "1" ]; then
             echo "$(date "+%Y-%m-%d %H:%M:%S") ${PRO_NAME} was killed"
-            bash bcash.sh
+            bash dogecoin.sh
             sleep 30
         fi
 
