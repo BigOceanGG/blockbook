01d650c7392323c3c5587ca21b350ecccfa7a152
ocean <3408598@qq.com>
2021-12-02 10:57:45 +0800

    add sh

---
+++ dogecoin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+FILE=~/coins/dogecoin/dogecoin_$(date "+%Y%m%d%H%M%S").log
+./blockbook -workers=1 -sync -blockchaincfg=build/dogecoin.json -datadir=/home/admin/coins/dogecoin/dbs -internal=:19005 -public=:19105 -logtostderr > ${FILE} 2>&1 &
+rm ~/coins/dogecoin/dogecoin.log
+ln -s ${FILE} ~/coins/dogecoin/dogecoin.log
