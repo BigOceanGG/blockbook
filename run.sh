a734e82ba0216a0753521701b8c4cf98355d312d
ocean <3408598@qq.com>
2021-12-06 09:35:19 +0800

    add sh

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+  echo "para error"
+  exit 1
+fi
+
+
+dir="log"
+if [ ! -d "$dir" ];then
+  mkdir $dir
+fi
+
 FILE=watch_$(date "+%Y%m%d%H%M%S")
 
-nohup bash watchx.sh >${FILE}.log &
+nohup bash watchx.sh $1 > log/${FILE}.log &
 rm watch.log
-ln -s ${FILE}.log watch.log
+ln -s log/${FILE}.log watch.log
--- watchx.sh
+++ watchx.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-PRO_NAME=dogecoin.json
+if [ $# -eq 0 ]; then
+  echo "para error"
+  exit 1
+fi
+
+PRO_NAME=$1.json
 export LD_LIBRARY_PATH=./
 
 while true; do
         NUM=`ps aux | grep ${PRO_NAME} | grep -v grep |wc -l`
         if [ "${NUM}" -lt "1" ]; then
             echo "$(date "+%Y-%m-%d %H:%M:%S") ${PRO_NAME} was killed"
-            bash dogecoin.sh
+            bash $1.sh
             sleep 30
         fi
 
