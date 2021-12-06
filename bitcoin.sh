0c9fcffc5d1e07a06e6a0967d4fc9dd2cff6c656
ocean <3408598@qq.com>
2021-12-06 10:05:47 +0800

    add sh

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FILE=~/coins/bitcoin/bitcoin_$(date "+%Y%m%d%H%M%S").log
-./blockbook -workers=1 -sync -resyncIndexPeriodMs=300000 -blockchaincfg=build/bitcoin.json -datadir=/home/admin/coins/bitcoin/dbs -internal=:19000 -public=:19100 -dbcache=1073741824 -logtostderr > ${FILE} 2>&1 &
+./blockbook -workers=1 -sync -resyncindexperiod=300000 -blockchaincfg=build/bitcoin.json -datadir=/home/admin/coins/bitcoin/dbs -internal=:19000 -public=:19100 -dbcache=1073741824 -logtostderr > ${FILE} 2>&1 &
 rm ~/coins/bitcoin/bitcoin.log
 ln -s ${FILE} ~/coins/bitcoin/bitcoin.log
