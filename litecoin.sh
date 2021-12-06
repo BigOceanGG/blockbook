3ddbb3969b4b9a19f1e16ba14a0496e42b080fec
ocean <3408598@qq.com>
2021-12-06 10:02:06 +0800

    add sh

--- litecoin.sh
+++ litecoin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FILE=~/coins/litecoin/litecoin_$(date "+%Y%m%d%H%M%S").log
-./blockbook -workers=1 -sync -blockchaincfg=build/litecoin.json -datadir=/home/admin/coins/litecoin/dbs -internal=:19004 -public=:19104 -logtostderr > ${FILE} 2>&1 &
+./blockbook -workers=1 -sync -resyncIndexPeriodMs=30000 -blockchaincfg=build/litecoin.json -datadir=/home/admin/coins/litecoin/dbs -internal=:19004 -public=:19104 -logtostderr > ${FILE} 2>&1 &
 rm ~/coins/litecoin/litecoin.log
 ln -s ${FILE} ~/coins/litecoin/litecoin.log
