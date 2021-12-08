d0732f6372f3186d44ae637f82f5225e5ba4b767
ocean <3408598@qq.com>
2021-12-08 10:11:45 +0800

    add sh

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-dir="/home/ubuntu/bitcoin-22.0/log"
+dir="/home/ubuntu/bitcoin-0.21.1/log"
 if [ ! -d "$dir" ];then
   mkdir $dir
 fi
 
-FILE=~/bitcoin-22.0/log/bitcoin_$(date "+%Y%m%d%H%M%S").log
-./blockbook -workers=1 -sync -resyncindexperiod=300000 -blockchaincfg=build/bitcoin.json -datadir=/home/ubuntu/bitcoin-22.0/dbs -internal=:19000 -public=:19100 -logtostderr > ${FILE} 2>&1 &
-rm ~/bitcoin-22.0/bitcoin.log
-ln -s ${FILE} ~/bitcoin-22.0/bitcoin.log
+FILE=~/bitcoin-0.21.1/log/bitcoin_$(date "+%Y%m%d%H%M%S").log
+./blockbook -workers=1 -sync -resyncindexperiod=300000 -blockchaincfg=build/bitcoin.json -datadir=/home/ubuntu/bitcoin-0.21.1/dbs -internal=:19000 -public=:19100 -logtostderr > ${FILE} 2>&1 &
+rm ~/bitcoin-0.21.1/bitcoin.log
+ln -s ${FILE} ~/bitcoin-0.21.1/bitcoin.log
